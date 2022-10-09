267e4aeda2ef952d2a4773200225de4a1e3aba48
Maria Lucila Mango <mango.mlucila@gmail.com>
2022-10-09 17:42:31 -0300

    Agrego views

--- ProyectoFinal/ProyectoFinal/urls.py
+++ ProyectoFinal/ProyectoFinal/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from ProyectoFinal.views import saludo
+
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
