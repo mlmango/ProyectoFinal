7964e56a493108249fa7c2ee6c4dd8b3dbf79b52
Maria Lucila Mango <mango.mlucila@gmail.com>
2022-10-09 16:30:32 -0300

    Agrego models

---
+++ ProyectoFinal/Blog/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from Blog.models import Autores, Articulos, Secciones
+
+# Register your models here.
+admin.site.register(Autores)
+admin.site.register(Articulos)
+admin.site.register(Secciones)
---
+++ ProyectoFinal/Blog/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Autores(models.Model):
+    class Meta:
+        verbose_name_plural = "Autores"
+
+    nombre = models.CharField(max_length=100)
+    username = models.CharField(max_length=50)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.username
+
+
+class Articulos(models.Model):
+    class Meta:
+        verbose_name_plural = "Articulos"
+
+    titulo = models.CharField(max_length=100)
+    texto = models.CharField(max_length=1000)
+    articulo_nro = models.IntegerField()
+    username_autor = models.CharField(max_length=100)
+    fecha_de_publicacion = models.DateField(null=True)
+
+
+class Secciones(models.Model):
+    class Meta:
+        verbose_name_plural = "Secciones"
+
+    nombre = models.CharField(max_length=30)
