726545420c0206b72bfddaacc014d52c45ac8f01
Maria Lucila Mango <mango.mlucila@gmail.com>
2022-10-09 17:29:23 -0300

    Agrego forms

---
+++ ProyectoFinal/Blog/forms.py
@@ -0,0 +1,18 @@
+from mailbox import NoSuchMailboxError
+from unittest.util import _MAX_LENGTH
+from django import forms
+
+class formulario_Autores(forms.Form):
+    nombre=forms.CharField(max_length=100)
+    username=forms.CharField(max_length=100)
+    email=forms.EmailField()
+
+class formulario_Articulos(forms.Form):
+    titulo=forms.CharField(max_length=100)
+    texto=forms.CharField(max_length=1000)
+    articulo_nro=forms.IntegerField()
+    username_autor=forms.CharField(max_length=100)
+    fecha_de_publicacion=forms.DateField(null=True)
+
+class formulario_Secciones(forms.Form):
+    nombre=forms.CharField()
