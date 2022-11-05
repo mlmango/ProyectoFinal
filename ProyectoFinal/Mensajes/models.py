b8d2b3554cad3f83526e60011a14aaa83fb7958a
mtruelse <truelsegaardmariana@gmail.com>
2022-11-05 12:23:00 -0300

    edito app de mensajes

--- ProyectoFinal/Mensajes/models.py
+++ ProyectoFinal/Mensajes/models.py
@@ -1,3 +1,9 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Message(models.Model):
+    destinatario = models.ForeignKey(User, on_delete=models.CASCADE)
+    mensaje = models.CharField(max_length=100)
